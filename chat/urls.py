c0a72de901d6326d1548fe6f57a1d02503d497a3
ctOS <darkprinceofpersia@rambler.ru>
2018-10-23 22:27:56 +0300

    Modified html and py

--- chat/urls.py
+++ chat/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.ctOS, name='ctOS'),
      url(r'^info/$', views.info, name='info'),
      url(r'^info_full/$', views.info_full, name='info_full'),
      url(r'^command/$', views.command, name='command'),
      url(r'^command_full/$', views.command_full, name='command_full'),
+     url(r'^chat$', views.chat, name='chat'),
 ]
