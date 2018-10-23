b36a0f137e80493d22c566fd41c30d0142b8e368
ctOS <darkprinceofpersia@rambler.ru>
2018-10-23 21:01:16 +0300

    New html

--- chat/urls.py
+++ chat/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+     url(r'^info/$', views.info, name='info'),
+     url(r'^info_full/$', views.info_full, name='info_full'),
+     url(r'^command/$', views.command, name='command'),
+     url(r'^command_full/$', views.command_full, name='command_full'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'chat/post_list.html', {})
+
+def info(request):
+    return render(request, 'chat/info.html', {})
+
+def info_full(request):
+    return render(request, 'chat/info_full.html', {})
+
+def command_full(request):
+    return render(request, 'chat/command_full.html', {})
+
+def command(request):
+    return render(request, 'chat/command.html', {})
