2cc6bae417727afa68bcf135969f844c3f313265
ctOS <darkprinceofpersia@rambler.ru>
2018-10-29 01:01:36 +0300

    chat v.1

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ctOS, name='ctOS'),
+     url(r'^$', views.ctOS, name='ctOS'),
      url(r'^info/$', views.info, name='info'),
      url(r'^info_full/$', views.info_full, name='info_full'),
      url(r'^command/$', views.command, name='command'),
@@ -12,6 +12,29 @@ urlpatterns = [
      url(r'^admini/$', views.admini, name='admini'),
 
 
-     url(r'^id1338/$', views.id1338, name='id1338'),
-     url(r'^id9395/$', views.id9395, name='id9395'),
+     url(r'^id6957/$', views.id6957, name='id6957'), #+
+     url(r'^id9750/$', views.id9750, name='id9750'), #+
+     url(r'^id0085/$', views.id0085, name='id0085'), #+-
+     url(r'^id0024/$', views.id0024, name='id0024'), #+-
+
+     url(r'^id2303/$', views.id2303, name='id2303'), #+
+     url(r'^id9395/$', views.id9395, name='id9395'), #+
+
+     url(r'^id5572/$', views.id5572, name='id5572'), #+
+     url(r'^id3998/$', views.id3998, name='id3998'), #+
+     url(r'^id1338/$', views.id1338, name='id1338'), #+
+
+     url(r'^id7557/$', views.id7557, name='id7557'), #+-
+
+     url(r'^id4575/$', views.id4575, name='id4575'), #+-
+     url(r'^id7540/$', views.id7540, name='id7540'),
+     url(r'^id7939/$', views.id7939, name='id7939'),
+     url(r'^id5886/$', views.id5886, name='id5886'),
+
+     url(r'^id1124/$', views.id1124, name='id1124'),
+     url(r'^id5452/$', views.id5452, name='id5452'),
+     url(r'^id8794/$', views.id8794, name='id8794'),
+
+     url(r'^id0787/$', views.id0787, name='id0787'),
+
 ]
--- chat/views.py
+++ chat/views.py
@@ -30,9 +30,39 @@ def admini(request):
 
 
 
-
-def id1338(request):
-    return render(request, 'chat/id1338.html', {})
-
+def id6957(request):
+    return render(request, 'chat/id6957.html', {})
+def id9750(request):
+    return render(request, 'chat/id9750.html', {})
+def id0085(request):
+    return render(request, 'chat/id0085.html', {})
+def id0024(request):
+    return render(request, 'chat/id0024.html', {})
+def id2303(request):
+    return render(request, 'chat/id2303.html', {})
 def id9395(request):
     return render(request, 'chat/id9395.html', {})
+def id5572(request):
+    return render(request, 'chat/id5572.html', {})
+def id3998(request):
+    return render(request, 'chat/id3998.html', {})
+def id1338(request):
+    return render(request, 'chat/id1338.html', {})
+def id7557(request):
+    return render(request, 'chat/id7557.html', {})
+def id4575(request):
+    return render(request, 'chat/id4575.html', {})
+def id7540(request):
+    return render(request, 'chat/id7540.html', {})
+def id7939(request):
+    return render(request, 'chat/id7939.html', {})
+def id5886(request):
+    return render(request, 'chat/id5886.html', {})
+def id1124(request):
+    return render(request, 'chat/id1124.html', {})
+def id5452(request):
+    return render(request, 'chat/id5452.html', {})
+def id8794(request):
+    return render(request, 'chat/id8794.html', {})
+def id0787(request):
+    return render(request, 'chat/id0787.html', {})
